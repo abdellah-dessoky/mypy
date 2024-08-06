0f9c65426c571e7b0ab6d015a728af9ecc5d80af
abdellah-dessoky <abdellah03desoky@gmail.com>
2024-08-06 17:31:34 +0300

    update

---
+++ basics/operations.py
@@ -0,0 +1,11 @@
+x= 10
+y = 3
+print("the division x and y = ")
+print( (x/y))
+print("the floored division x and y = ")
+print( (x//y))
+"""
+/ normal divison which is going to show floating point if there is 
+// floored division which is not going to show the floating point in division 
+
+"""
