9dc5e912bf6f0417fb4364de9b14701ae3d4fdfa
abdellah-dessoky <abdellah03desoky@gmail.com>
2024-08-07 16:12:24 +0300

    final update

--- basics/operations.py
+++ basics/operations.py
@@ -5,15 +5,21 @@ print( (x/y))
 print("the floored division x and y = ")
 print( (x//y))
 # strings operations 
-
 str = 'test message '
 number = 3
 print(number * str)
 print("number " * 5)
 # input operations 
+name = input('Name : \n')
 print("Enter a message to be printed")
 inp = input()
-print(inp)
+print("The message you entered is : "+inp)
+# if the user entered a float or a string it would throw exception 
+favnum= input('What is your favourite number? \n')
+print('your favourite number is my favourite too '+favnum)
+print('Thanks for printing with us '+name+ '!')
+
+# printing with us lol. reminds me of atm after i deposit money "thanks for banking with us"
 """
 / normal divison which is going to show floating point if there is 
 // floored division which is not going to show the floating point in division 
