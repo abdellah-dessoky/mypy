4a8565239d89821a6511a539ef267475ef43d26f
abdellah-dessoky <abdellah03desoky@gmail.com>
2024-08-07 15:45:30 +0300

    operations update

--- basics/operations.py
+++ basics/operations.py
@@ -4,8 +4,20 @@ print("the division x and y = ")
 print( (x/y))
 print("the floored division x and y = ")
 print( (x//y))
+# strings operations 
+
+str = 'test message '
+number = 3
+print(number * str)
+print("number " * 5)
+# input operations 
+print("Enter a message to be printed")
+inp = input()
+print(inp)
 """
 / normal divison which is going to show floating point if there is 
 // floored division which is not going to show the floating point in division 
+- other mathmatical operations are the same 
+- when a string is multiplied with number it will be printed x times 
 
 """
